940015b0521892107c7262bc0ce37f2e7c8cfb70
Enes Can Türkoğlu <enescan58turko@gmail.com>
2023-01-22 21:20:33 +0300

    v0.0.11 - Incomplete Type
    Incomplete Type

--- MacOS/Cpp_code/Cpp_code/main.cpp
+++ MacOS/Cpp_code/Cpp_code/main.cpp
@@ -1,10 +1,8 @@
-/* DEFAULT ARGUMENTS
-
- - Varsayılan argüman öyle bir mekanizmadır ki fonksiyon bildiriminde yapılan bir belirtmeyle, fonksiyon çağrısı sırasında argüman gönderilmeyen parametre değişkenlerine, fonksiyon bildirimi veya tanımlaması sırasında, daha önceden, geçilmiş olan varsayılan argüman değerleriyle fonksiyona çağrıda bulunulması.
- - Yani, ben bu parametreye argüman göndermiyorum, sevgili derleyici bildirimde belirtilen argümanla bu fonksiyonu çağır
- - Fonksiyon bildirilerinde fonksiyon parametresine geçilecek olan "varsayılan argümanlar" sabit, değişken, fonksiyon geribildirimleri gibi ifadeler olabilir.
- - Varsayılan argümanlar istenilen parametreden verilmeye başlanılabilir fakat varsayılan argüman verilen parametrenin sağındaki bütün parametrelere de varsayılan argüman geçilmelidir. Solundaki parametrelere verilme zorunluluğu yoktur.
- - Eğer varsayılan argümanlar fonksiyonun bildiriminde geçilmişse, fonksiyon tanımında tekrardan yazılması syntax error'a sebep olur. Bunun tersi de geçerlidir. Yani varsayılan argümanlar ya bildirimde ya da tanımda geçilebilir.
+/* INCOMPLETE TYPE (FORWARD) DECLERATION:
+ 
+ - Derleyici bir türün varlığından haberdar fakat o türün tanımıyla(örneğin yapının elemanlarıyla ) karşılaşmamışsa bu tür incomplete type'dır.
+ - Incomplete türünden nesne yaratılamaz çünkü derleyicinin nesne yaratabilmesi için boyutunu bilmesi gerekmektedir.
+ 
 */
 
 
@@ -12,72 +10,23 @@
 #include <iostream>
 
 
+// Yapılar
+struct NECO;    // incomplete type, tamamlanmamis tur
+
+
 
 // Func
 
-
-// 1
-int func ( int x, int y = 10);
-
-// 2
-int func_2 ( int x = 5, int y = 10 );
-
-// 3 - HATA
-int func_3 ( int x = 10, int y );
-
-// 4
-int foo();
-int func_4 ( int x, int y = foo());
-
-// 5
-int ab;
-int func_5 ( int x, int y = ab);
-
-// 6
-int g = 10;
-int func_5 ( int &r = g);
-
-
-// 8
-int func_8 ( int d = 100, int m = 200, int y = 300);
-
-
-// 7 - HATA, redefiniton of default argument
-int func_7 ( int d = 10, int m = 1, int y = -1);
-
-int func_7 ( int d = 10, int m = 1, int y = -1)
-{
-    
-}
-
-// 9
-int func_9 ( int , int , int );
-
-int func_9 ( int d = 10, int m = 1, int y = -1)
-{
-    
-}
-
-
-// 10
-int func_10 ( int , int = 3, int = 45);
-
-int func_10 ( int d , int m , int y )
-{
-    
-}
-
-
+struct NECO foo(struct NECO *);   // GECERLI
+typedef struct NEOCO  necati;     // GECERLI
 
 
 //
 int main()
 {
+    struct NECO *p;   // GECERLI
     
-    func_8();           // d = 100, m = 200, y = 300
-    func_8(1);          // d = 1,   m = 200, y = 300
-    func_8(1,2);        // d = 1,   m = 2  , y = 300
-    func_8(1,2,3);      // d = 1,   m = 2  , y = 3
-    
-    
+    struct NECO x;    // HATALI, Derleyicinin nesne olusturabilmesi icin yer ayırması lazım. Bu yüzden de NECO'nun boyutunu bilmesi gerekir
+
+    int x = sizeof(struct NECO);   // HATALI, Incomplite type kullanilamaz
 }
