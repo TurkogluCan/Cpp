92b95a6184add1c281b5592176c348a34c84f118
Enes Can Türkoğlu <enescan58turko@gmail.com>
2023-01-22 21:02:38 +0300

    v0.0.10 - Default Argument
    Default Argument

--- MacOS/Cpp_code/Cpp_code/main.cpp
+++ MacOS/Cpp_code/Cpp_code/main.cpp
@@ -1,103 +1,83 @@
+/* DEFAULT ARGUMENTS
 
-/* CONSTEXPR
- *************************************
- - Sabit ifadeler, derleyicinin koda bakarak doğrudan bir sabit elde ettiği ifadelere denir. Yani
-   ifadenin değeri runtime değil compiler time'da bellidir.
- 
- - *** CONSTEXPR fonksiyon; girdileri, içerisinde yapılan işlemler ve dönüşü sabit olan fonksiyonlara denir. Bu tip fonksiyonların
-   içeriği sabit olduğu için fonksiyonun geri dönüş değeri de diğer sabitler gibi runtime'da değil de compiler time'da elde edilir.
- - *** constexpr fonksiyonların çıktıları compiler time'da elde edildiği için bu fonksiyonun içerisine yazılan kodlar gizlenemez. Eğer
-   lib yapılmak istenirse veya fonksiyon başka dosyalardan çağırılacaksa başlık dosyasına yazılmalıdır ki derleyici compiler time'da sonucu üretebilsin.
-   *** Bu yüzden CONSTEXPR FONKSİYONLARIN İÇERİĞİ KESİNLİKLE GİZLENEMEZ.
- - constexpr fonksiyonlar inline fonksiyonlardır.
- 
- - constexpr belirteci ile yapılan tanımlamalar sabit olarak kabul edilir.
- - constexpr ile tanımlanan değişkenlerin kullanıldığı ifadeler de birer sabit ifadeleridir.
- 
- **************************************/
+ - Varsayılan argüman öyle bir mekanizmadır ki fonksiyon bildiriminde yapılan bir belirtmeyle, fonksiyon çağrısı sırasında argüman gönderilmeyen parametre değişkenlerine, fonksiyon bildirimi veya tanımlaması sırasında, daha önceden, geçilmiş olan varsayılan argüman değerleriyle fonksiyona çağrıda bulunulması.
+ - Yani, ben bu parametreye argüman göndermiyorum, sevgili derleyici bildirimde belirtilen argümanla bu fonksiyonu çağır
+ - Fonksiyon bildirilerinde fonksiyon parametresine geçilecek olan "varsayılan argümanlar" sabit, değişken, fonksiyon geribildirimleri gibi ifadeler olabilir.
+ - Varsayılan argümanlar istenilen parametreden verilmeye başlanılabilir fakat varsayılan argüman verilen parametrenin sağındaki bütün parametrelere de varsayılan argüman geçilmelidir. Solundaki parametrelere verilme zorunluluğu yoktur.
+ - Eğer varsayılan argümanlar fonksiyonun bildiriminde geçilmişse, fonksiyon tanımında tekrardan yazılması syntax error'a sebep olur. Bunun tersi de geçerlidir. Yani varsayılan argümanlar ya bildirimde ya da tanımda geçilebilir.
+*/
+
 
 // Include
 #include <iostream>
 
 
+
 // Func
-constexpr int sum_square(int x, int y)
+
+
+// 1
+int func ( int x, int y = 10);
+
+// 2
+int func_2 ( int x = 5, int y = 10 );
+
+// 3 - HATA
+int func_3 ( int x = 10, int y );
+
+// 4
+int foo();
+int func_4 ( int x, int y = foo());
+
+// 5
+int ab;
+int func_5 ( int x, int y = ab);
+
+// 6
+int g = 10;
+int func_5 ( int &r = g);
+
+
+// 8
+int func_8 ( int d = 100, int m = 200, int y = 300);
+
+
+// 7 - HATA, redefiniton of default argument
+int func_7 ( int d = 10, int m = 1, int y = -1);
+
+int func_7 ( int d = 10, int m = 1, int y = -1)
 {
-    return x*x + y*y;
+    
 }
+
+// 9
+int func_9 ( int , int , int );
+
+int func_9 ( int d = 10, int m = 1, int y = -1)
+{
+    
+}
+
+
+// 10
+int func_10 ( int , int = 3, int = 45);
+
+int func_10 ( int d , int m , int y )
+{
+    
+}
+
+
 
 
 //
 int main()
 {
-    // Sabit ifadeler, dizi tanımlaması sırasında index constexpr ifadeler ile yapılmıştır. Const anahtar sözcüğü ile de geçerlidir.
-    // Bu ifade C'de geçersizdir.
-    {
-        constexpr int x = 10;
-        constexpr int y = 20;
-        
-        int a[x+y]{};   // Dogru
-    }
     
-    //** Const anahtar sözcüğü kullanarak sabit olmayan ifadelerle yapılan tanımlamanın sonucu da const değildir. Fakat hata da değildir.
-    {
-        int x = 10;
-        int y = 20;
-        const int b = x+y;
-        int a[b]{};             // HATA
-    }
+    func_8();           // d = 100, m = 200, y = 300
+    func_8(1);          // d = 1,   m = 200, y = 300
+    func_8(1,2);        // d = 1,   m = 2  , y = 300
+    func_8(1,2,3);      // d = 1,   m = 2  , y = 3
     
-    //** Constexpr anahtar sözcüğü kullanarak sabit olmayan ifadelerle yapılan tanımlama GEÇERSİZDİR.
-    {
-        int x = 10;
-        int y = 20;
-        constexpr int b = x+y;        // HATA
-    }
-    
-    // Constexpr örnek ifade
-    {
-        const int x = 10;
-        const int y = 20;
-        constexpr int b = x*x + y*y;        // GEÇERLİ
-    }
-    
-    // Constexpr örnek ifade
-    {
-        constexpr int x = 10;
-        constexpr int y = 20;
-        constexpr int b = x*x + y*y;        // GEÇERLİ
-    }
-    
-    //** CONST vs CONSTEXPR
-    //   constexpr anahtar sözcüğü kullanılarak yapılan ilk değer atamasında kullanılan fonksiyon mutlaka "constexpr fonksiyon" olmalıdır.
-    //   Bu zorunluluk const için yoktur.
-    {
-        int foo();
-        
-        const int x = foo();          // GEÇERLİ
-
-        constexpr int y = foo();      // HATA
-    }
-    
-    //** CONSTEXPR fonksiyon, girdileri, içerisinde yapılan işlemler ve dönüşü sabit olan fonksiyonlara denir. Bu tip fonksiyonların
-    // içeriği sabit olduğu için fonksiyonun geri dönüş değeri de diğer sabitler gibi runtime'da değil de compiler time'da elde edilir.
-    // Eğer fonksiyon içerisinde sabitliği bozan ifadeler kullanılırsa hata verir ve geçersizdir.
-    {
-        const int a = 2;
-        const int b = 3;
-
-        constexpr int y = sum_square(a+5, b*2);   // GECERLİ, Derleyici için sabit bir ifadedir.
-        int arr[sum_square(a+5, b*2)]{};          // Sabit bir ifade olduğu için dizinin boyutu da yapılabilir
-    }
-    
-    //** CONSTEXPR fonksiyon, HATALI
-    {
-        int a = 2;
-        int b = 3;
-
-        constexpr int y = sum_square(a,b);   // HATALI, fonksiyon parametreleri sabit olmalı
-    }
     
 }
-
-
