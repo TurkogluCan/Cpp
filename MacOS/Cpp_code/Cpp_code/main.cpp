1b283375569705b586c73b40d2eb3bbe0518561a
Enes Can Türkoğlu <enescan58turko@gmail.com>
2023-01-29 18:07:01 +0300

    v0.0.15 - Funtion Overloading
    Funciton overloading giriş seviye örnekleri

--- MacOS/Cpp_code/Cpp_code/main.cpp
+++ MacOS/Cpp_code/Cpp_code/main.cpp
@@ -1,97 +1,74 @@
-/* FUNTION OVERLOADING(Fonksiyon Yüklemesi)
- 
- 
- Funtion Overloading Giriş:
- -------------------------
- Funtion overloading tanımına girmdeden önce şöyle bir örnek verelim.
- 
- Aritmektik işlem operatörü olan '+' işaretinin kullanımı sırasında;
-    Eğer 'int'   türünden değişkenlerle toplama yaparsam arka planda üreteceği makina kod farklı,
-    Eğer 'float' türünden değişkenlerle toplama yaparsam arka planda üreteceği makina kod farklı olacaktır.
- Bu durumda derleyici bize tamsayılar için farklı gerçek sayılar için farklı bir operatör kullacaksın dese saçma ve gereksiz olurdur.
- İşte function overloading de buna benzemektedir.
- 
- 
- Funtion Overloading Genel Tanım:
- --------------------------------
- Aynı isme sahip ve aynı işlevi gerçekleştiren, farklı türden ve sayıdan parametre bildirilere sahip olan fonksiyonların tanımlanmasına ve kullanılmasına "Fonksiyon Overloading" denir. Funciton overolading'in ana kullanım amacı, ana işlevleri benzer olan fonksiyonların, parametre değişikliğine bağlı olarak farklılaşmasını engellemek, aynı isimle overload ederek farklı fonksiyonlar gibi kullanabilmektir. Overload edilen fonksiyonların içeriği birbirlerinden farklı olabilir ama fonksiyonun amacı aynı olmalı ki kullanıcı tarafından hatalı kullanım olmasın.
- 
- ---------------------------
-
- Fonksiyon yüklemesinden söz edilebilmesi için iki koşulun sağlanması gerekir:
-
- 1) Aynı isimli birden fazla fazla fonksiyon aynı kapsamda (scope) var olmalı.
- 2) Fonksiyonların imzaları farklı olmalıdır.
-
- 
- Function Signatur(Fonksiyon İmzası):
- ------------------------------------
- Bir fonksiyonun geri dönüş türü hariç, fonksiyonun ismi, parametre tipleri ve parametre sayısını belirten bilgiye "fonksiyonun imzası" denir.  Fonksiyonun geri dönüş değerinin türünü fonksiyonun imzasının bir parçası olarak görmeyeceğiz
-    
- // Imzaları aynı
-         int func(int)
-         double func(int);
-
- 
- Scope Kuralları:
- ----------------
- Bildirilen fonksiyonların scope'ları aynı olmalıdır. Eğer scopelar farklıysa Function Overloadingden BAHSEDİLEMEZ.
- 
- //Scope Tipleri
-    Namespace scope (file scope in C)
-    Class     scope
-    Block     scope
-    Function prototype scope
-    Function scope
-    
- Token(Atom) ve Name Lookup(İsim Arama):
- ---------------------------------------
- Derleyici açısından kaynak kodun en küçük birimidir. Örneğin isimler. Derleyici kodu atomize eder. Kodun derlenme sırasında derleyicinin bir isimle karşılaşıp bu ismi araması durumuna "Name Lookup" denir.
- 
- 
- İsim arama kuralları:
- 
- 1)
- 
- Derleyici ismi ararken önce kendi scope'una, sonra bütün kapsayan scope'lara, eğer yine bulamazsa bütün global olanlarda arar.
- Fakat C++'da "Scope Resolution, ::" operatörü kullanılarak derleyiciye, derleyici sen bu ismi global alanda ara diyerek farklı bir scope araması yaptırabiliriz.
- 
- ------------
- int a;             // Global alan
- 
- int main()
- {
-    float a = 1.5f;
- 
-    cout << a;        // Burada derleyici öncelikle kendi scope'una bakacağı için gloabelde tanımlı olan a'nın hiçbir önemi yoktur.
- }
- 
- ------------
- 
- int main()
- {
-    int printf = 10;
- 
-    printf("enescan");     // Burada derleyici öncelikle kendi scope'una bakacağı için standart kütüphanede
-                           // tanımlı olan printf'in hiçbir hiçbir önemi yoktur.
- }
-
+/* FUNTION OVERLOADING(Fonksiyon Yüklemesi).2
+  - Funciton overloading giriş seviye örnekleri
 */
 
 
 // Include
 #include <iostream>
-
-
 // Yapılar
 
-
 // Func
+int foo(int, int);
+ foo(int, int);
 
 
 //
 int main()
 {
+
+    // - Function redeclaration, overloading değil
+    {
+        int foo(int, int);
+        int foo(int, int);
+    }
     
+    // - Hatalı kullanım, imzaları aynı ama geri dönüşü farklı
+    {
+        int    foo(int, int);
+        double foo(int, int);
+    }
+    
+    // - CONST: Kullanıcıyı korumak yoluyla yapılan constlar, top level constlar, imzayı değiştirmez.
+    //  Bu yüzden aşağıdakiler overloading değil, redeclaration'dır.
+    {
+        int foo(const int);
+        int foo(int);
+        
+        int func(int *);
+        int func(int *const);
+    }
+    
+    // + const: "Const Overloading"
+    {
+        int func(const int *);
+        int func(int *);
+        
+        int foo(const int &);
+        int foo(int &);
+    }
+    
+    // - Default Arg, Function redeclaration
+    {
+        int func(int x);
+        int func(int x=10);
+    }
+    
+    // + Default arg
+    {
+        int func(int x);
+        int func(int x, int y=10);
+    }
+   
+    // + Referans, ÇOK TEHLİKELI
+    {
+        int func(int &x);
+        int func(int x );
+    }
+    
+    // + Referans ve pointer
+    {
+        int func(int &);
+        int func(int *);
+    }
    
 }
