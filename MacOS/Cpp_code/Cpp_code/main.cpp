d6d279fb8ff0cb21367a3eaba2f7aed2965b836b
Enes Can Türkoğlu <enescan58turko@gmail.com>
2023-02-05 22:04:39 +0300

    v0.0.18 - Classes
    Class ve namespace

--- MacOS/Cpp_code/Cpp_code/main.cpp
+++ MacOS/Cpp_code/Cpp_code/main.cpp
@@ -1,27 +1,60 @@
-/* Type Casting (Tur Donusumu)
+/* Classes
     
- C'de kullanılmakta olan casting operatörleri C++'da geçerli olmakla birlikte önerilmez. C++'da tür dönüşümleri için özelleşmiş tür dönüşüm operatörleri bulunmaktadır. Bunlar;
- 
- 1-) Static Cast ( static_cast<T>(ifade), Örn. T = int )
- 
- - Static tür dönüşümleri için kullanılmaktadır. Mesela aritmetik türler arasındaki dönüşümler için kullanılır. int->double, double->int, long->char vb. kullanımları static tür dönüşümüne örnektir. Enum typelar ile aritmetik türler arasında olan veya enum typeların kendileri arasındaki dönüşümleri de statik tür dönüşümlerine birer örnektir.
- - İstisna olarak (void *)->(T *) olan dönüşümler de statik tür dönüşümüne örnektir.
- 
- 
- 2-) Const Cast ( const_cast<T>(ifade), T = char, char * )
- 
- - cv qualified(bir türün const veya volatile anahtar sözcüğü ile nitelenmiş hali) olarak nitelenmiş ifadelerin constluğunu kaldırmak için kullanılır.
- - const_cast işlemcisi, işlem yapılan nesnenin sabit ve/veya volatile özelliğini kaldırır veya yeniden kazandırır. Aslında, bir nesnenin const özelliğini kaldırma, o nesnenin bellekteki değerini değiştirmeden, sadece const olmayan bir değer kabul edilen bir ifade veya fonksiyon parametresinde kullanılmasını sağlar.
- 
- 
- 3-)Reinterpret Cast ( reinterpret_cast <T> (ifade) )
- 
- - reinterpret_cast işlemcisi bir veri türünü tamamen farklı bir veri türüne dönüştürür.
+ Class Elemanları
+ ----------------
+ class Data
+ {
+     int mx;             // Data member
+     void func(int);     // Member function
+     typedef int Word;   // Type member(member type, nested type)
+ };
+
+ Class Scope
+ -----------
+ Bir sınıfın tanımı içerisinde bildirilen tüm isimler "class scope" denilen özel bir kapsama sahiptir.
  
  
- 4-) Dynamic Cast
+ Şu durumlarda bir isim class scope içinde aranır(Name lookup);
  
- - Inheritance kısmında değinilecektir.
+ 1) "."  operatörünün sağ operandı olan isimler
+ 2) "->" operatörünün sağ operandı olan isimler
+ 3) "::" (scope resolution) operatörünün sağ operandı olan isimler
+ 
+Unary: Tek operantlı scope resolution kullanımı. Eğer isimle ilgili bir işlemde, örneğin int tipinden bir değişken, unary çözünürlük operatörü kullanılırsa bu, derleyiciye ismi namespace scope'ta, global alanda ara demek olur(name lookup aşaması). Yerel scope'ta x değişkeni, ismi yer alsa bile global alanda arama yapılır.
+ Örn. "::x"
+
+ //
+ int g = 10;
+
+ int main()
+ {
+     int g = 20;
+     
+     ::g += g;           // Global alandaki g(::g(Unary)) ile localdeki g'nin toplamı
+ }
+ //
+ 
+Binary: Çift operantlı scope resolution kullanımı. Binary çözünürlük operatörünün sol operandı olan isim ya bir namespace ismi, ya da bir class ismidir. Sağ operandındaki isim ise buna bağlı olarak class veya ilgili namespace scope'ta aranacaktır.
+Örn. "a::b"
+ 
+ 
+ Namespace (İsim Alanı)
+ ----------------------
+ İsimleri birbirinden gizlemek için kullanılan bir araçtır. Gloabal alandaki isimlerin birbirleriyle karışmasını engellemek için oluşturulmuş isim bölgeleridir. Namespace global alanı parselleyerek kendine özgü isim alanları yaratır.
+
+ namespace Neco
+ {
+     int x = 64;
+ }
+
+ //
+ int main()
+ {
+     int x = 1;
+
+     std::cout << x << '\n';         // x = 1;
+     std::cout << Neco::x << '\n';   // x = 64;
+ }
 
 */
 
@@ -31,11 +64,25 @@
 // Yapılar
 
 
+class Data
+{
+    int mx;             // Data member
+    void func(int);     // Member function
+    typedef int Word;   // Type member(member type, nested type)
+};
+
+
+
+namespace Neco
+{
+    int x = 64;
+}
 
 //
 int main()
 {
+    int x = 1;
 
-    
-    
+    std::cout << x << '\n';         // x = 1;
+    std::cout << Neco::x << '\n';   // x = 64;
 }
