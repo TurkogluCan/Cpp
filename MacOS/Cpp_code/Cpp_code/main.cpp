c5aeddadbd9f7944ead54ec02f99deee889d9c9a
Enes Can Türkoğlu <enescan58turko@gmail.com>
2023-03-19 16:26:29 +0300

    v0.0.23 - Scope Leakage
    Scope Leakage

--- MacOS/Cpp_code/Cpp_code/main.cpp
+++ MacOS/Cpp_code/Cpp_code/main.cpp
@@ -1,95 +1,21 @@
-/* const member func, mutable, default member initialize
+/* Scope Leakage
+ 
+ ->i: Morden C++ ile birlikte if deyiminde scope leakage ile ilgili bir yenilik gelmiştir. Artık sadece if deyimlerinin içerisinde kullanılacak değişkenler, if koşulundan önce tanımlanabilmekte, ilk değer atanabilmekte ve koşul kontrolü yapılabilmektedir.
  
  
- Const Member Function
- ---------------------
- ->i: Gizli parametrenin const olması istenen durumlarda, fonksiyonun sınıf içerisindeki ve tanımalama sırasındaki bildirimi sırasında bildirimin sonuna "const" anahtar sözcüğü koyulur. Eğer koyulmazsa default olarak const olmayan bir gizli parametreye sahip fonksyion yaratılır. Const koyulan fonksiyon üyelerini "get(...)", koyulmayanları ise birer "set(...)" fonksiyonlar olarak düşünebiliriz.
- 
- //
- class Data {
- private:
-     int mx;
-     
- public:
-     void getfunc(int x)const;
-     void setfunc(int x);
- };
-
-
-
- void Data::setfunc(int x)
+ if (int x = foo(); x > 14)
  {
-     mx = x;
+     x = 1;
  }
-
- void Data::getfunc(int x)const
+ else if (x > 25)
  {
-     mx = x;    // HATA
+     x = 2;
+ }
+ else
+ {
+     x = 3;
  }
  
- 
- // 2
- class Data {
- private:
-     int mx;
-     
- public:
-     Data *foo();
-     Data &func();
- };
-
- Data* Data::foo()
- {
-     //...
-     return this;
- }
-
- Data& Data::func()
- {
-     //...
-     return *this;
- }
- 
- 
- mutable
- -------
- ->i: Sınıfın bir veri elemanını mutable anahtar sözüğü ile tanımlamak şu anlama gelir, kodu okuyan sevgili kişi, bu veri elemanının değişmesi, sınıf türünden yaratılan bir nesnenin problem domainindeki anlamını değiştirmez. Yani bu değişse de değişmese de seni etkilemez. Örneğin sınıf içerisinde kullanılan ve dışarıyı etkilemeyen sayaçlar
- ->i: Mutable data memberlar const functionlar içeriside değiştirilebilir.
- 
- //
- class Data {
- private:
-     mutable int msayac;
-     
- public:
-     void getfunc(int x)const;
-     void setfunc(int x);
- };
-
- void Data::getfunc(int x)const
- {
-     msayac++;    // Dogru
- }
-
- 
- Default member initializer
- ---------------------------
- ->i: Modern c++ ile birlikte gelen bir kullanımdır. Bir sınıf nesnesinin hayata getirilmesi durumunda onun veri elemanına ilk değer verilebilir.
- 
- //
- class Data {
- private:
-     int mx = 10;;
-     
- public:
-     Data &f1();
-     Data &f2();
-     Data &f3();
-     
-     int gmx = 20;
- };
- 
-
 */
 
 
@@ -99,43 +25,14 @@
 
 
 //
-class Data {
-private:
-    int mx = 10;;
-    
-public:
-    Data &f1();
-    Data &f2();
-    Data &f3();
-    
-    int gmx = 20;
-};
 
 
 
-
-Data& Data::f1()
-{
-    //...
-    return *this;
-}
 
 
 //
 int main()
 {
-    Data mydata;
-
-    // Bu kullanım geçerli ve doğrudur. Fonksiyon dönüşün nesnenin referansı olduğu için o referansla
-    // tekrar farklı bir fonksiyon çağrısı yapmak veya data membera ulaşmak da mümkündür.
-    mydata.f1().f2().f3();
-    mydata.f1().f2().f3().gmx = 10;
+   
     
-    // mydata.f1() dönüşü -> mydata, bu yüzden dönüş üzerinden başka bir membera ulaşmak mümkündür.
-    
-    //
-    {
-
-    }
-
 }
