ea26b4f9aacdeae8c24824af1d4e0d221ed13b8b
Enes Can Türkoğlu <enescan58turko@gmail.com>
2023-04-09 16:02:59 +0300

    v0.0.28 - Copy Constructor
    Copy Constructor

--- MacOS/Cpp_code/Cpp_code/main.cpp
+++ MacOS/Cpp_code/Cpp_code/main.cpp
@@ -1,107 +1,103 @@
-/* COTR Bildirim Çeşitleri
+/* Copy Constructor
  
  
- Doesn't Exist
+ Copy Constructor
  -------------
- Programlayıcı ve derleyici tarafından yazılmamış, olmayan COTR durumu
  
- Aşağıdaki durumda programlayıcı parametreli COTR prototipini yazmış fakat bildirmemiştir. Bu durumda derleyici parametreli bir COTR bulunduğu için default COTR de yazmayacaktır.
+Bir sınıf nesnesi eğer hayata aynı bir türden bir başka sınıf nesnesinden değer alarak gelecekse bu durumda sınıfın copy contructor denen özel üye fonksiyonu çağırılır. Her sınıfın mutlaka copy constructor'ı bulunur.
+ 
+class MyClass
+ {
+ public:
+     MyClass()
+     {
+         std::cout << "Default CTOR \n";
+     };
+     ~MyClass()
+     {
+         std::cout << "Destructor \n";
+     };
+ };
+ 
+ //main
+ MyClass mx;         // Default Constructor çağırılır
+ MyClass my = mx;    // Default Constructor çağırılmaz, copy constructor çağırılır
+
+ -->i: mx için default constructor çağırılır fakat my için default değil copy contstructor çağırılır. O yüzden consola sadece 1 defa default COTR yazdırılır. Ömürleri bittiğinde ise 2 defa Destructor yazdırılır.
+ 
+ -->i: Aşağıdaki copy constructor atamaları özdeştir.
+ 
  //
+ MyClass mx;
+ MyClass my = mx;
+ MyClass mz(mx);
+ MyClass mt{mx};
+
+ 
+ -->i: Fonksiyonların aynı sınıf türünden olan parametrelerine olan çağrılar da copy constructorları çağırır.
+ 
+ void func(MyClass myclas)
+ {
+     
+ }
+ 
+ int main()
+ {
+     MyClass mx;         // Default Constructor çağırılır
+     func(mx);           // Copy Constructor çağırılır
+ }
+ 
+ 
+ -->i: Fonksiyonların aynı sınıf türünden olan return dönüşleri için de geçici olarak yaratılan nesnelerin copy constructorları çağırılır.
+ 
+ MyClass foo();
+ 
+ 
+ ***
+ --i: Eğer derleyici bizim için bir copy constructor yazarsa derleyicinin yazdığı COTR sınıfın;
+ * non-static
+ * public
+ * inline üye fonksiyonu olacaktır.
+ 
+ 
+ Derleyici tarafından yazılacak olan örnek copy ctor
+
  class MyClass
  {
  public:
-     MyClass(int);
+     MyClass (const MyClass &r);
  };
  
+ -->i: Eğer programlayıcının işini görüyorsa copy ctor yazma işi derleyiciye bırakılır.
+ -->i: Aşağıdaki şekilde programlayıcı tarafından copy constructor yazılabilir. Aşağıdaki copy cotr, derleyicinin yazacağıyla aynıdır. Programlayıcı düzenleyebilir.
  
- ->i: Programlayıcının COTR bildirmediği durumlarda derleyici tarafından otomatik olarak COTR bildirilir. Bunlar;
- 
-    DERLEYICI TARAFINDAN
- --------------------------------
- Implicitly Declared Defaulted  |
- Implicitly Declared Deleted    |
- --------------------------------
- 
- Implicitly Declared Defaulted:
- ------------------------------
- Derleyici, sınıfın özel üye fonksiyonunu(COTR) bizim için bildirir.
- 
- 
- Eğer aşağıdaki MyClass kullanıcı tarafından oluşturulmazsa derleyici tarafından aşağıdaki şekilde default olarak oluşturulur
  //
  class MyClass
  {
+ private:
+     int a,b,c;
  public:
-     MyClass() { }
- };
-
+     MyClass (const MyClass &other) : a(other.a), b(other.b), c(other.c)
+     {
+         
+     }
  
  
- Implicitly Declared Deleted:
- ----------------------------
- Derleyici, sınıfın özel üye fonksiyonunu(COTR) bizim için deleted olarak bildirir. Bu fonksiyona yapılan çağrı syntax hatasıdır.
- *** Örneğin sınıfın üye elemanlarında birisi cont olsun. Eğer const üye elemana sahip bir sınıf varsa COTR list initialize sırasında ilk değer atanması lazımdır. Eğer programlayıcı COTR yazmamışsa derleyici default COTR yazar fakat const ataması yapılmadığı için syntax hatası vereceğinden bunu delete eder.
- 
- 
- Eğer aşağıdaki MyClass kullanıcı tarafından oluşturulmazsa derleyici tarafından aşağıdaki şekilde default olarak oluşturulur ve yine derleyici tarafından delete edilir
- //
- class MyClass
- {
-    const int cmx;
- public:
-     MyClass() = delete;
- };
-
-
- Programlayıcı tarafından bildirilen COTR fonksiyonları aşağıdaki yöntemlerle bildirilebilir.
-
- PROGRAMLAYICI TARAFINDAN
--------------------------
-User Declared            |
-User Declared Defaulted  |
-User Declared Deleted    |
-User Declared Defined    |
--------------------------
- 
- User Declared
- -------------
- Programlayıcı tarafından bildirilen COTR'lardır.
+ -->i: Aynı türden sınıflara ait nesnelerin hayata getirilirken kopyalanması, Copy constructor delete edilerek syntax hatası haline getirilebilir.
  //
  class MyClass
  {
  public:
      MyClass();
+     MyClass (const MyClass &) = delete;
  };
- 
- User Declared Defaultted
- -------------------------
- Programlayıcı tarafından bildirilen fakat derleyiciye yazdırılan COTR'lardır.
- //
- class MyClass
+
+ int main()
  {
- public:
-     MyClass() = default;
- };
- 
- User Declared Deleted
- -------------------------
- Programlayıcı tarafından bildirilen fakat yapılan çağrıların syntax hatası olduğu COTR'lardır.
- //
- class MyClass
- {
- public:
-     MyClass() = delete;
- };
- 
- 
- 
- -->i: Bir sınıftan türetilmiş pointer veya referans'lar için constructor çağrısında bulunulmaz çünkü nesne değillerdir, nesneyi gösterirler.
- 
- MyClass  mx;            // CTOR çağırılır
- MyClass  &r{mx};        // CTOR çağırılmaz
- MyClass  *p = &mx;      // CTOR çağırılmaz
- 
- 
+     MyClass mx;
+     MyClass my=mx;  // HATA: Call to deleted constructor of "MyClass"
+     
+ }
  
  */
 
@@ -115,13 +111,16 @@ User Declared Defined    |
 class MyClass
 {
 public:
-    MyClass() = delete;
+    MyClass();
+    MyClass (const MyClass &) = delete;
 };
+
+
 
 //
 int main()
 {
-
-    
+    MyClass mx;
+    MyClass my=mx;  // HATA: Call to deleted constructor of "MyClass"
     
 }
